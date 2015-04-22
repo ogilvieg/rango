8da82a8d8376c48b2b460042c6d0219f1ef60bac
Gawaine O\'Gilvie <gawaine.ogilvie@gmail.com>
2015-04-22 07:40:18 -0400

    working models examples. completed.

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-	name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 class Page(models.Model):
     category = models.ForeignKey(Category)
